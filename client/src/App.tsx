1db16e03d16b08b3558c81ede9b2f8afeb216a3a
Harsh kumar <2001kumarharsh@gmail.com>
2025-05-01 00:40:09 +0530

    new packages installed and updates context/ routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,36 +3,40 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "./components/ThemeProvider";
-import { ResumeProvider } from "./context/ResumeContext";
+import { ResumeProvider } from "./contexts/ResumeContext";
+import { AuthProvider } from "./contexts/AuthContext";
 import Index from "./pages/Index";
 import Results from "./pages/Results";
 import NotFound from "./pages/NotFound";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-
+import About from "./pages/About";
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <ThemeProvider defaultTheme="system">
       <TooltipProvider>
-        <ResumeProvider>
-          <Toaster position="top-center" />
-          <BrowserRouter
-            basename="/"
-            window={window}
-            future={{ v7_startTransition: true }}
-          >
-            <Routes>
-              <Route path="/" element={<Index />} />
-              <Route path="/results" element={<Results />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<Signup />} />
-              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </BrowserRouter>
-        </ResumeProvider>
+        <AuthProvider>
+          <ResumeProvider>
+            <Toaster position="top-center" />
+            <BrowserRouter
+              basename="/"
+              window={window}
+              future={{ v7_startTransition: true }}
+            >
+              <Routes>
+                <Route path="/" element={<Index />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/results" element={<Results />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </BrowserRouter>
+          </ResumeProvider>
+        </AuthProvider>
       </TooltipProvider>
     </ThemeProvider>
   </QueryClientProvider>
