7b8a3ee347d567618ac89a98d6fd343946909c5b
Harsh kumar <2001kumarharsh@gmail.com>
2025-05-02 10:58:14 +0530

    update path and requirements.txt

--- server/main.py
+++ server/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from api.routes.resume import router as resume_router
+from routes.resume import router as resume_router
 import os
 
 app = FastAPI(title="PromptCV API")
